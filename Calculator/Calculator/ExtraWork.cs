c41a381fe0df9ee68eabafa263381e0e0870cdbf
Sadik Hassan <sadikhassan234@gmail.com>
2021-03-28 13:37:16 +0600

    Test Interface 1 & 2

---
+++ Calculator/Calculator/ExtraWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    interface ExtraWork
+    {
+        void XtoY(double a, double b);
+        void cuberoot(double x);
+        void Sqroot(double y);
+    }
+}
